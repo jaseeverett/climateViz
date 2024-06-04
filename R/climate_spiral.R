76169d3d324b5c8d11387f11769d5ff16c9cbf72
Jason Everett <jaseeverett@gmail.com>
2024-06-04 19:56:57 +1000

    Allow plot to run from single codebase

--- R/climate_spiral.R
+++ R/climate_spiral.R
@@ -40,169 +40,181 @@ climate_spiral <- function(data = NULL, by_year = FALSE, animate = FALSE) {
     dplyr::slice_max(.data$month_number)
 
 
-  month_labels <- tibble::tibble(
-    x = 1:12,
-    labels = month.abb,
-    y = 2.7
-  )
-
-  gridlines <- tibble::tibble(
-    x = c(1.2, 1.3, 1.6),
-    xend = c(12.8, 12.7, 12.4),
-    y = c(1, 0, -1),
-    yend = .data$y
-  )
-
-
   if (isTRUE(by_year)) {
 
-    temp_lines <- tibble::tibble(
-      x = 12,
-      y = c(1.5, 2.0),
-      labels = c("1.5\u00B0C", "2.0\u00B0C")
-    )
-
-    gg <- gg_dat %>%
-      ggplot2::ggplot(ggplot2::aes(x = .data$month_number,
-                                   y = .data$t_diff,
-                                   group = .data$Year,
-                                   color = .data$Year)) +
-      ggplot2::geom_col(
-        data = month_labels,
-        ggplot2::aes(x = .data$x + 0.5, y = 2.4), fill = "black",
-        width = 1,
-        inherit.aes = FALSE
-      ) +
-      ggplot2::geom_col(
-        data = month_labels,
-        ggplot2::aes(x = .data$x + 0.5, y = -2), fill = "black",
-        width = 1,
-        inherit.aes = FALSE
-      ) +
-      ggplot2::geom_hline(yintercept = c(1.5, 2.0), color = "red") +
-      ggplot2::geom_line() +
-      ggplot2::geom_point(
-        data = annotation,
-        ggplot2::aes(x = .data$month_number,
-                     y = .data$t_diff,
-                     color = .data$Year),
-        size = 2,
-        inherit.aes = FALSE
-      ) +
-      ggplot2::geom_label(
-        data = temp_lines,
-        ggplot2::aes(x = .data$x,
-                     y = .data$y,
-                     label = .data$labels),
-        color = "red", fill = "black", label.size = 0,
-        inherit.aes = FALSE
-      ) +
-      ggplot2::geom_text(
-        data = month_labels,
-        ggplot2::aes(x = .data$x, y = .data$y, label = .data$labels),
-        inherit.aes = FALSE, color = "white",
-        angle = seq(360 - 360 / 12, 0, length.out = 12)
-      ) +
-      # ggplot2::geom_text(ggplot2::aes(x = 1, y = -1.3, label = "2022"), size = 6) +
-      ggplot2::scale_x_continuous(
-        breaks = 1:12,
-        labels = month.abb, expand = c(0, 0),
-        sec.axis = ggplot2::dup_axis(name = NULL, labels = NULL)
-      ) +
-      ggplot2::scale_y_continuous(
-        breaks = seq(-2, 2, 0.2),
-        limits = c(-2, 2.7), expand = c(0, -0.7),
-        sec.axis = ggplot2::dup_axis(name = NULL, labels = NULL)
-      ) +
-      ggplot2::scale_color_viridis_c(
-        breaks = seq(min(.data$Year), max(.data$Year), 20),
-        guide = "none"
-      ) +
-      ggplot2::coord_polar(start = 2 * pi / 12) +
-      ggplot2::labs(
-        x = NULL,
-        y = NULL,
-        title = "Global temperature change (1880-2022)"
-      ) +
-      ggplot2::theme(
-        panel.background = ggplot2::element_rect(fill = "#444444", size = 1),
-        plot.background = ggplot2::element_rect(fill = "#444444", color = "#444444"),
-        panel.grid = ggplot2::element_blank(),
-        axis.text.x = ggplot2::element_blank(),
-        axis.text.y = ggplot2::element_blank(),
-        axis.title.y = ggplot2::element_blank(),
-        axis.ticks = ggplot2::element_blank(),
-        axis.title = ggplot2::element_text(color = "white", size = 13),
-        plot.title = ggplot2::element_text(color = "white", hjust = 0.5, size = 15)
-      )
-  }
-
-  if (isFALSE(by_year)) {
+    clm <- "Year" # Column name
+    lims <- c(-1.2, 2.5)
+    grids <- c(0, 1.5, 2)
+    clr <- "red"
 
     temp_lines <- tibble::tibble(
       x = 1,
-      y = c(1, 0, -1),
-      labels = c("+1\u00B0 C", "0\u00B0 C", "-1\u00B0 C")
+      y = grids,
+      labels = stringr::str_c(as.character(grids), "\u00B0 C")
     )
 
-    gg <- gg_dat %>%
-      ggplot2::ggplot(ggplot2::aes(x = .data$month_number,
-                                   y = .data$t_diff,
-                                   group = .data$Year,
-                                   color = .data$t_diff)) +
-      ggplot2::geom_line() +
-      ggplot2::geom_segment(
-        data = gridlines, ggplot2::aes(x = .data$x,
-                                       y = .data$y,
-                                       xend = .data$xend,
-                                       yend = .data$yend),
-        color = c("yellow", "green", "yellow"),
-        inherit.aes = FALSE
-      ) +
-      ggplot2::geom_col(
-        data = month_labels,
-        ggplot2::aes(x = .data$x + 0.5, y = -2), fill = "black",
-        width = 1,
-        inherit.aes = FALSE
-      ) +
-      ggplot2::geom_text(
-        data = temp_lines,
-        ggplot2::aes(x = .data$x, y = .data$y, label = .data$labels),
-        color = c("yellow", "green", "yellow"), size = 2, fontface = "bold",
-        inherit.aes = FALSE
-      ) +
-      ggplot2::geom_text(
-        data = month_labels,
-        ggplot2::aes(x = .data$x, y = .data$y, label = .data$labels),
-        inherit.aes = FALSE, color = "yellow"
-      ) +
-      ggplot2::scale_y_continuous(
-        limits = c(-2.0, 1.5),
-        expand = c(0, -0.3)
-      ) +
-      ggplot2::scale_color_gradient2(
-        low = "blue", high = "red", mid = "white", midpoint = 0,
-        guide = "none"
-      ) +
-      ggplot2::coord_polar(start = 0) +
-      ggplot2::labs(title = glue::glue("Global temperature change ({min(gg_dat$Year)}-{max(gg_dat$Year)})"),
-                    x = NULL,
-                    y = NULL) +
-      ggplot2::theme(
-        panel.background = ggplot2::element_rect(fill = "black"),
-        plot.background = ggplot2::element_rect(fill = "black", color = "black"),
-        panel.grid = ggplot2::element_blank(),
-        axis.text.x = ggplot2::element_blank(),
-        axis.text.y = ggplot2::element_blank(),
-        axis.ticks = ggplot2::element_blank(),
-        axis.title = ggplot2::element_blank(),
-        plot.title = ggplot2::element_text(
-          color = "white",
-          margin = ggplot2::margin(b = 5, t = 10, unit = "pt"),
-          hjust = 0.05
-        ),
-      )
   }
+
+
+
+  if (isFALSE(by_year)) {
+
+    clm <- "t_diff" # Column name
+    grids <- c(1, 0, -1)
+    lims <- c(-1.2, 1.5)
+    clr <- c("yellow", "green", "yellow")
+
+    temp_lines <- tibble::tibble(
+      x = 1,
+      y = grids,
+      labels = stringr::str_c(as.character(grids), "\u00B0 C")
+    )
+
+  }
+
+  month_labels <- tibble::tibble(
+    x = 1:12,
+    labels = month.abb,
+    y = max(lims) * 1.1
+  )
+
+  # if (isTRUE(legend)){
+  #   plotguide <- "colourbar"
+  # } else if (isFALSE(legend)){
+  #   plotguide <- "none"
+  # }
+
+
+  gg <- gg_dat %>%
+    ggplot2::ggplot(ggplot2::aes(x = .data$month_number,
+                                 y = .data$t_diff,
+                                 group = .data$Year,
+                                 color = .data[[clm]])) +
+    ggplot2::geom_col(
+      data = month_labels,
+      ggplot2::aes(x = .data$x + 0.5, y = min(lims)), fill = "black",
+      width = 1,
+      inherit.aes = FALSE) +
+    ggplot2::geom_col(
+      data = month_labels,
+      ggplot2::aes(x = .data$x + 0.5, y = max(lims)), fill = "black",
+      width = 1,
+      inherit.aes = FALSE) +
+    ggplot2::geom_line() +
+    {if (by_year == FALSE) ggplot2::scale_color_gradient2(low = "blue", high = "red", mid = "white", midpoint = 0, guide = "none")} +
+    {if (by_year == TRUE) ggplot2::scale_color_viridis_c(breaks = seq(min(gg_dat$Year), max(gg_dat$Year), 20), guide = "none")} +
+    ggplot2::geom_hline(
+      yintercept = grids,
+      color = clr) +
+    ggplot2::geom_text(
+      data = temp_lines,
+      ggplot2::aes(x = .data$x, y = .data$y, label = .data$labels),
+      color = clr, size = 6, fontface = "bold",
+      inherit.aes = FALSE) +
+    ggplot2::geom_text(
+      data = month_labels,
+      ggplot2::aes(x = .data$x,
+                   y = .data$y,
+                   label = .data$labels),
+      inherit.aes = FALSE,
+      color = "white",
+      angle = seq(0, 360 - 360 / 12, length.out = 12) * -1) +
+    ggplot2::scale_y_continuous(
+      limits = c(min(lims), max(lims) * 1.2),
+      expand = c(0, 0)) +
+    ggplot2::coord_radial(expand = FALSE, inner.radius = 0, start = 0) +
+    ggplot2::labs(title = glue::glue("Global temperature change ({min(gg_dat$Year)}-{max(gg_dat$Year)})"),
+                  x = NULL,
+                  y = NULL) +
+    ggplot2::theme(
+      panel.background = ggplot2::element_rect(fill = "#444444", size = 1),
+      # panel.background = ggplot2::element_rect(fill = "black", size = 1),
+      plot.background = ggplot2::element_rect(fill = "#444444", color = "#444444"),
+      panel.grid = ggplot2::element_blank(),
+      axis.text.x = ggplot2::element_blank(),
+      axis.text.y = ggplot2::element_blank(),
+      axis.ticks = ggplot2::element_blank(),
+      axis.title = ggplot2::element_blank(),
+      plot.title = ggplot2::element_text(
+        color = "white",
+        margin = ggplot2::margin(b = 5, t = 10, unit = "pt"),
+        hjust = 0.05
+      ),
+    )
+
+
+  #
+  # gg <- gg_dat %>%
+  #   ggplot2::ggplot(ggplot2::aes(x = .data$month_number,
+  #                                y = .data$t_diff,
+  #                                group = .data$Year,
+  #                                color = .data$Year)) +
+  #   ggplot2::geom_col(
+  #     data = month_labels,
+  #     ggplot2::aes(x = .data$x + 0.5, y = 2.4), fill = "black",
+  #     width = 1,
+  #     inherit.aes = FALSE
+  #   ) +
+  #   ggplot2::geom_col(
+  #     data = month_labels,
+  #     ggplot2::aes(x = .data$x + 0.5, y = -2), fill = "black",
+  #     width = 1,
+  #     inherit.aes = FALSE
+  #   ) +
+  #   ggplot2::geom_hline(yintercept = c(1, 1.5, 2.0), color = "red") +
+  #   ggplot2::geom_line() +
+  #   ggplot2::geom_point(
+  #     data = annotation,
+  #     ggplot2::aes(x = .data$month_number,
+  #                  y = .data$t_diff,
+  #                  color = .data$Year),
+  #     size = 2,
+  #     inherit.aes = FALSE
+  #   ) +
+  #   ggplot2::geom_label( # Add degree change labels
+  #     data = temp_lines,
+  #     ggplot2::aes(x = .data$x,
+  #                  y = .data$y,
+  #                  label = .data$labels),
+  #     color = "red",
+  #     fill = "black",
+  #     label.size = 0,
+  #     inherit.aes = FALSE
+  #   ) +
+  #   ggplot2::geom_text(
+  #     data = month_labels,
+  #     ggplot2::aes(x = .data$x,
+  #                  y = .data$y,
+  #                  label = .data$labels),
+  #     inherit.aes = FALSE,
+  #     color = "white",
+  #     angle = seq(360 - 360 / 12, 0, length.out = 12)
+  #   ) +
+  #   # ggplot2::geom_text(ggplot2::aes(x = 1, y = -1.3, label = "2022"), size = 6) +
+  #   ggplot2::scale_x_continuous(
+  #     breaks = 1:12,
+  #     labels = month.abb,
+  #     expand = c(0, 0),
+  #     sec.axis = ggplot2::dup_axis(name = NULL, labels = NULL)
+  #   ) +
+  #   ggplot2::scale_y_continuous(
+  #     breaks = seq(-2, 2, 0.2),
+  #     limits = c(-2, 2.7), expand = c(0, -0.7),
+  #     sec.axis = ggplot2::dup_axis(name = NULL, labels = NULL)
+  #   ) +
+  #   ggplot2::scale_color_viridis_c(
+  #     breaks = seq(min(gg_dat$Year), max(gg_dat$Year), 20),
+  #     guide = "none"
+  #   ) +
+  #   ggplot2::coord_polar(start = 2 * pi / 12) +
+  #   ggplot2::labs(
+  #     x = NULL,
+  #     y = NULL,
+  #     title = "Global temperature change (1880-2022)"
+  #   )
+
+
+
 
   if (isTRUE(animate)) {
 
@@ -234,4 +246,5 @@ climate_spiral <- function(data = NULL, by_year = FALSE, animate = FALSE) {
   } else {
     return(gg)
   }
+
 }
